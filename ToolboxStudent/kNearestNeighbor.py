876d00358bd35854a38fdc27734db615fbd249e2
Lukas L <ak01ahez@stud.informatik.uni-erlangen.de>
2016-11-02 16:03:26 +0100

    first version

--- ToolboxStudent/kNearestNeighbor.py
+++ ToolboxStudent/kNearestNeighbor.py
@@ -27,16 +27,80 @@ import numpy
 
 
 class kNearestNeighbor(object):
-
 	def __init__(self, k):
-		return None
+		self.__mMax = 1e8
+		self.__k = k
 
 
 	def fit(self, X, y):
-		return None
+		# store references to the labeled training data
+		self.__X = X
+		self.__y = y
+		self.__m = len(X)
+		self.__mMax = 1e8
+		self.printDebug()
 
 
 	def predict(self, X):
-		return None
+		# |x-y|^2 = (x - y)^T (x - y) = - 2 * x^T y + x^T x + y^T y 
+		# runtime efficient as for-loops are avoided, but runs out of memory 
+		# pretty fast for large training and test sets;
+		# process only m test samples at a time
 
+		m = int(self.__mMax / self.__m)
+		numRuns = math.ceil(len(X) / m)
+		
+		# number of different classes
+		numC = int(max(self.__y))
+
+		
+		Z = numpy.zeros(0)
+		for i in range(numRuns):
+		    Xs = X[i * m: (i + 1) * m]
+		    d1 = numpy.square(Xs).sum(axis = 1)
+		    d2 = numpy.square(self.__X).sum(axis = 1)
+		    D = numpy.dot(Xs, self.__X.T)
+		    D *= -2
+		    D += d1.reshape(-1, 1)
+		    D += d2
+		    
+		    temp = numpy.argsort(D, axis=1)
+		    #indexes ind of k closest points for each point
+		    ind = numpy.argsort(D, axis = 1)[:, 0:self.__k]
+		    
+		    #array Z of fixed size to store output to w/o memory-costly append/insert functions
+		    Z = numpy.zeros(len(ind)) 
+		    #saves the classes of the corresponding k closest points (saved in 2D array ind) to 2D array T
+		    T = (numpy.array(self.__y[ind])).astype(int) 
+		    
+		    j = 0
+		    #index = (self.__m) - 1
+		    
+		    for sub in T:
+		    	#sums up the occurence of certain classes in T
+		    	binc = numpy.bincount(sub)
+		    	#sorts the classes according to number of occurence -> most occurende in last index (-1) -> gives back class with most occurence of the k neighboring points
+		    	a = (numpy.argsort(binc)[-1]).astype(int)
+		    	Z[j] = a
+		    	j +=1
+		    j = 0
+
+
+		return Z   
+	
+
+
+
+	def printDebug(self):
+		print("__k: ", self.__k, " __mMax: ", self.__mMax, " __X: ", self.__X, " __y: ", self.__y, " __m: ", self.__m)
+		
+
+
+
+		
+#print(" numRuns: ", numRuns, "\n"," i: ", i, "\n"," lenX: ", len(X), "\n"," X: ", X, "\n", " Xs: ", Xs, "\n"," d1: ", d1, "\n", " d2: ", d2, "\n", " D: ", D, "\n", " temp: ", temp, "\n", " ind: ", ind, "\n", " T: ", T, "\n", " Z: ", Z, "\n")
+
+
+
+	
 	
