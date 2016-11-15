3bd7e292269475e32964b3ed2ecee221c598bf52
Lukas L <you@example.com>
2016-11-15 14:06:02 +0100

    start LinearLogisticRegression

--- ToolboxStudent/LinearLogisticRegression.py
+++ ToolboxStudent/LinearLogisticRegression.py
@@ -29,18 +29,51 @@ import numpy.matlib
 class LinearLogisticRegression(object):
 
     def __init__(self, learningRate = 0.5, maxIterations = 100):
+		self.__learningRate = learningRate
+		self.__maxIterations = maxIterations
         return None
 
 
+	#estimate from labeled training samples
     def fit(self, X, y):
+		self.__X = X
+		self.__y = y
+		self.__m = len(X)
+
         return None
-
-
+        
+        
+    #sigmoid fct g(O)=1/(1+e^-Ox)  O=theta
+	# X vector
     def gFunc(self, X, theta):
-        return None
+		return  1 / (1 + math.exp(-(transpose(theta).dot(X))))      
 
-
+	#compute O (theta) with newton (from training samples) -> plug O in gFunc() -> check all classes y with gFunc and return highest posterior prob.
     def predict(self, X):
-        return None
+        
+		
+
+
+
+
+
+		return Z
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
